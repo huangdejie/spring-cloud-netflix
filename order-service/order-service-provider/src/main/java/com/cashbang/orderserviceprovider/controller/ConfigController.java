2409dd1a55de6e7e1550ef0c921682710d9970af
huangdj <790566366@qq.com>
2021-01-09 15:24:04 +0800

    config自动刷新

--- order-service/order-service-provider/src/main/java/com/cashbang/orderserviceprovider/controller/ConfigController.java
+++ order-service/order-service-provider/src/main/java/com/cashbang/orderserviceprovider/controller/ConfigController.java
@@ -1,6 +1,7 @@
 package com.cashbang.orderserviceprovider.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Author: huangdj
  * @Date: 2021/1/8
  */
+@RefreshScope
 @RestController
 public class ConfigController {
 
